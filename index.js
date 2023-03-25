fdd67652ff31124d062333faae7f55ce524b705f
Change Agent <keasiley@icloud.com>
2023-03-25 16:08:40 -0500

    Inquirer prompts added to index.js

--- index.js
+++ index.js
@@ -1,13 +1,111 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require = ('./utils/gererateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+   
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'Title',
+    },
+   
+    {
+      type: 'input',
+      message: 'Enter a description of your project',
+      name:'Description',
+    },
+   
+    {
+        type: 'input',
+        message: 'Please create a Table of Contents',
+        name: 'Table',
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    },
+   
+    {  
+        type: 'input',
+        message: 'Please provide installation instructions',
+        name: 'Installation',
+    
+    },
+   
+    {
+        type: 'input',
+        message: 'Please provide usage information',
+        name: 'Usage',
+    },
+   
+    {   
+        type: 'input',
+        message: 'Please provide usage gudielines',
+        name: 'Contributing',
+    },   
+    
+    {
+        type: 'input',
+        message: 'Please provide testing instructions',
+        name: 'Tests',
+    },
 
-// TODO: Create a function to initialize app
-function init() {}
+  ])
 
-// Function call to initialize app
-init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // TODO: Include packages needed for this application
+
+// // TODO: Create an array of questions for user input
+// const questions = [];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
